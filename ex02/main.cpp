6ff88be7d0b455e4abb31fec49eac9c6e4afb91b
HamzaDazIA <hamzadaziag@gmail.com>
2025-12-01 00:38:09 +0100

    ss

---
+++ ex02/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <list>
+#include "MutantStack.hpp"
+
+int main()
+{
+    // ==========================================
+    // PART 1: Your MutantStack
+    // ==========================================
+    std::cout << "--- MutantStack Output ---" << std::endl;
+    MutantStack<int> mstack;
+    
+    mstack.push(5);
+    mstack.push(17);
+    
+    std::cout << mstack.top() << std::endl; 
+    
+    mstack.pop(); 
+    
+    std::cout << mstack.size() << std::endl; 
+    
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    
+    ++it;
+    --it;
+    
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+
+    // ==========================================
+    // PART 2: std::list (Verification)
+    // ==========================================
+    std::cout << "\n--- std::list Output ---" << std::endl;
+    std::list<int> mlist;
+    
+    mlist.push_back(5); // push -> push_back
+    mlist.push_back(17);
+    
+    std::cout << mlist.back() << std::endl;
+    
+    mlist.pop_back(); 
+    
+    std::cout << mlist.size() << std::endl; 
+    
+    mlist.push_back(3);
+    mlist.push_back(5);
+    mlist.push_back(737);
+    mlist.push_back(0);
+    
+    std::list<int>::iterator lit = mlist.begin();
+    std::list<int>::iterator lite = mlist.end();
+    
+    ++lit;
+    --lit;
+    
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+    
+    return 0;
+}
